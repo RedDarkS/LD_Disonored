f782e3cf049a42a4656eeeef068d4e38f9677b9a
MickaelFP <72392674+MickaelFP@users.noreply.github.com>
2022-02-18 08:42:41 +0100

    FogTriggerInterExter+passerelle

---
+++ Assets/FogTriggering.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FogTriggering : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == ("Player"))
+        {
+            GestionFog.Instance.fogState = "Inter";
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == ("Player"))
+        {
+            GestionFog.Instance.fogState = "Exter";
+        }
+    }
+}
---
+++ Assets/GestionFog.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GestionFog : MonoBehaviour
+{
+    [SerializeField] GameObject FogExter;
+    [SerializeField] GameObject FogInter;
+
+    public string fogState = "Default";
+
+    public static GestionFog Instance;
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        fogState = "Exter";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        FogActivation();
+    }
+
+    void FogActivation()
+    {
+        if (fogState == "Exter")
+        {
+            FogExter.SetActive(true);
+            FogInter.SetActive(false);
+        }
+        else if (fogState == "Inter")
+        {
+            FogInter.SetActive(true);
+            FogExter.SetActive(false);
+        }
+    }
+}
